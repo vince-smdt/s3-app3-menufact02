8c0fa39d28f4972f19855f7a1cac7489509138d2
MAubin <63813705+mazacra@users.noreply.github.com>
2024-06-10 18:28:24 -0400

    JavaDoc pour chef

--- MenuFact02/src/main/Chef/Chef.java
+++ MenuFact02/src/main/Chef/Chef.java
@@ -5,13 +5,25 @@ import main.plats.PlatChoisi;
 
 import java.util.ArrayList;
 
+/**
+ * <p>Classe représentant un cuisinier</p>
+ */
 public class Chef {
     ArrayList<PlatChoisi> commandes;
 
+    /**
+     * <p>Constructeur de la classe</p>
+     */
     public Chef() {
         commandes = new ArrayList<>();
     }
 
+    /**
+     * <p>Méthode pour mettre a jour l'état d'un plat par un chef</p>
+     * @param iPlat L'index du plat a modifier
+     * @param etat Enumération des états (0=Commande, 1=En_preparation, 2=Termine, 3=Servi, 4=Impossible_de_servir)
+     * @throws ChefException classe de gestion des exceptions de la classe Chef
+     */
     public void changerEtatPlat(int iPlat, int etat) throws ChefException {
         ArrayList<PlatChoisi> nouveau = new ArrayList<>();
         for (int i = 0; i < commandes.size(); i++){
@@ -24,10 +36,18 @@ public class Chef {
         commandes = nouveau;
     }
 
+    /**
+     * <p>Méthode appelé par l'observateur lorsqu'il est déclanché. ELle ajoute le plat dans les commandes du chef</p>
+     * @param plat plat ajouté dans la facture
+     */
     public void update(PlatChoisi plat) {
         commandes.add(plat);
         System.out.println("Plat a preparer: " + plat.toString());
     }
 
+    /**
+     * <p>Méthode pour retourner la liste des plats du chefs<p/>
+     * @return liste des plats
+     */
     public ArrayList<PlatChoisi> getList(){ return commandes; }
 }
